19435f7649ca243390afbdb117fc1c27943a4432
PabloPisentte <145517667+PabloPisentte@users.noreply.github.com>
2023-10-18 12:05:54 -0300

    Add files via upload

---
+++ qpx.js
@@ -0,0 +1,19 @@
+function generar(){
+    var qpx=Math.floor((Math.random()*99)+1);
+}
+function quiniela(){
+    document.getElementById("1").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("2").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("3").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("4").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("5").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("6").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("7").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("8").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("9").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("10").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("11").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("12").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("13").value=Math.floor((Math.random()*99)+1);
+    document.getElementById("14").value=Math.floor((Math.random()*99)+1);
+}
